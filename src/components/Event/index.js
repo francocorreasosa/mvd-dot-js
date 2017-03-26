273d27415e2ee4075cce6306bb4ae1e5c1099cbc
Franco Correa <franco@francocorrea.com.uy>
2017-03-26 20:13:10 -0300

    Stateless components

--- src/components/Event/index.js
+++ src/components/Event/index.js
@@ -2,18 +2,16 @@ import React from 'react';
 import moment from 'moment';
 import './styles.css';
 
-export default class Event extends React.Component {
-  render() {
-    const { event } = this.props;
+export default (props) => {
+  const { event } = props;
 
-    return (
-      <div className="event">
-        <h1 className="event-name">{event.name.split('-')[0]}</h1>
-        <p className="event-details">{moment(event.time).format('llll')} – {event.yes_rsvp_count} javascripters {typeof(event.rating) !== 'undefined' ? ` – ${Math.round(event.rating.average)} star${Math.round(event.rating.average) == 1 ? '' : 's'} (${event.rating.count} review${event.rating.count == 1 ? '' : 's'})`: ''}</p>
-        <div 
-          dangerouslySetInnerHTML={{ __html: event.description }}
-          className="description"/>
-      </div>
-    );
-  }
+  return (
+    <div className="event">
+      <h1 className="event-name">{event.name.split('-')[0]}</h1>
+      <p className="event-details">{moment(event.time).format('llll')} – {event.yes_rsvp_count} javascripters {typeof(event.rating) !== 'undefined' ? ` – ${Math.round(event.rating.average)} star${Math.round(event.rating.average) == 1 ? '' : 's'} (${event.rating.count} review${event.rating.count == 1 ? '' : 's'})`: ''}</p>
+      <div 
+        dangerouslySetInnerHTML={{ __html: event.description }}
+        className="description"/>
+    </div>
+  );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import './styles.css';
 
-export default class Header extends React.Component {
-  render() {
-    return (
-      <header>
-        <div className="overlay">
-          <div className="content">
-            <h1 className="meetup-name">The Montevideo Javascript Meetup Group</h1>
-            <p>We just meet and talk about the amazing world of JS.</p>
-          </div>
-        </div>
-      </header>
-    );
-  }
-}
+export default (props) => (
+  <header>
+    <div className="overlay">
+      <div className="content">
+        <h1 className="meetup-name">The Montevideo Javascript Meetup Group</h1>
+        <p>We just meet and talk about the amazing world of JS.</p>
+      </div>
+    </div>
+  </header>
+);
