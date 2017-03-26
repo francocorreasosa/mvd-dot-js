3c79f3d315f06b05dbdb76b853d9ae27b0d94db5
Franco Correa <franco@francocorrea.com.uy>
2017-03-26 20:47:54 -0300

    Add address and venue name

--- src/components/Event/index.js
+++ src/components/Event/index.js
@@ -8,7 +8,7 @@ export default (props) => {
   return (
     <div className="event">
       <h1 className="event-name">{event.name.split('-')[0]}</h1>
-      <p className="event-details">{moment(event.time).format('llll')} – {event.yes_rsvp_count} javascripters {typeof(event.rating) !== 'undefined' ? ` – ${Math.round(event.rating.average)} star${Math.round(event.rating.average) == 1 ? '' : 's'} (${event.rating.count} review${event.rating.count == 1 ? '' : 's'})`: ''}</p>
+      <p className="event-details">{moment(event.time).format('llll')} - {event.venue.name} ({event.venue.address_1}) – {event.yes_rsvp_count} javascripters {typeof(event.rating) !== 'undefined' ? ` – ${Math.round(event.rating.average)} star${Math.round(event.rating.average) == 1 ? '' : 's'} (${event.rating.count} review${event.rating.count == 1 ? '' : 's'})`: ''}</p>
       <div 
         dangerouslySetInnerHTML={{ __html: event.description }}
         className="description"/>
