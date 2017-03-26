4e02b2cac9641541b2ecb8f62d97d3153df2b5f8
Franco Correa <franco@francocorrea.com.uy>
2017-03-26 20:07:11 -0300

    Improve design

--- src/components/Event/index.js
+++ src/components/Event/index.js
@@ -8,8 +8,8 @@ export default class Event extends React.Component {
 
     return (
       <div className="event">
-        <h2 className="event-name">{event.name.split('-')[0]}</h2>
-        <p>{moment(1354829400000).format('llll')} – {event.yes_rsvp_count} javascripters {typeof(event.rating) !== 'undefined' ? ` – ${Math.round(event.rating.average)} star${Math.round(event.rating.average) == 1 ? '' : 's'} (${event.rating.count} review${event.rating.count == 1 ? '' : 's'})`: ''}</p>
+        <h1 className="event-name">{event.name.split('-')[0]}</h1>
+        <p className="event-details">{moment(event.time).format('llll')} – {event.yes_rsvp_count} javascripters {typeof(event.rating) !== 'undefined' ? ` – ${Math.round(event.rating.average)} star${Math.round(event.rating.average) == 1 ? '' : 's'} (${event.rating.count} review${event.rating.count == 1 ? '' : 's'})`: ''}</p>
         <div 
           dangerouslySetInnerHTML={{ __html: event.description }}
           className="description"/>
