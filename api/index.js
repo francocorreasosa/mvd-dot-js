71bfa82a5432ba79493b12385df8e9cd823df2b7
Franco Correa <franco@francocorrea.com.uy>
2017-03-28 21:04:00 -0300

    Use envionment variables for meetup URLs

--- api/index.js
+++ api/index.js
@@ -7,14 +7,14 @@ const app = express();
 app.use(cors());
 
 app.get('/past', (req, res) => {
-  axios.get('https://api.meetup.com/2/events?offset=0&format=json&limited_events=False&group_urlname=montevideojs&photo-host=public&page=500&fields=&order=time&status=past&desc=false&sig_id=186661001&sig=aa8e77c37668ac24b7d8fb6af2f6bf0b226c5b57')
+  axios.get(process.env.PAST_MEETUPS_URL)
        .then(response => response.data)
        .then((data) => { res.json(data.results) })
        .catch(res.json);  
 });
 
 app.get('/upcoming', (req, res) => {
-  axios.get('https://api.meetup.com/2/events?offset=0&format=json&limited_events=False&group_urlname=montevideojs&photo-host=public&page=500&fields=&order=time&status=upcoming&desc=false&sig_id=186661001&sig=6de2859bce5fda77c47f4cebfb43479bb0508e3a')
+  axios.get(process.env.UPCOMING_MEETUPS_URL)
        .then(response => response.data)
        .then((data) => { res.json(data.results) })
        .catch(res.json);
