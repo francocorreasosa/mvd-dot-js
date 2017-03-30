06c910bcd608b75b3bcb8f6ae72255302c9a4261
Franco Correa <franco@francocorrea.com.uy>
2017-03-30 19:21:45 -0300

    Show propose a talk component in an empty event

--- src/components/Event/index.js
+++ src/components/Event/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import moment from 'moment';
 import './styles.css';
+import ProposeTalk from '../ProposeTalk';
 
 export default (props) => {
   const { event } = props;
-
   return (
     <div className="event">
       <h1 className="event-name">{event.name.split('-')[0]}</h1>
-      <p className="event-details">{moment(event.time).format('llll')} - {event.venue.name} ({event.venue.address_1}) – {event.yes_rsvp_count} javascripters {typeof(event.rating) !== 'undefined' ? ` – ${Math.round(event.rating.average)} star${Math.round(event.rating.average) == 1 ? '' : 's'} (${event.rating.count} review${event.rating.count == 1 ? '' : 's'})`: ''}</p>
+      <p className="event-details">{moment(event.time).format('llll')} - {event.venue.name} ({event.venue.address_1}) – {event.yes_rsvp_count} javascripters {typeof(event.rating) !== 'undefined' ? ` – ${Math.round(event.rating.average)} star${Math.round(event.rating.average) === 1 ? '' : 's'} (${event.rating.count} review${event.rating.count === 1 ? '' : 's'})`: ''}</p>
       <div 
         dangerouslySetInnerHTML={{ __html: event.description }}
         className="description"/>
+      {!event.description && 
+        <ProposeTalk />
+      }
+
     </div>
   );
 }
